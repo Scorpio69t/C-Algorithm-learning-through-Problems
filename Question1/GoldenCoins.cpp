8e4fca9bc951802b9cc3e83df75476897f063c40
Mr yang <18728163854@163.com>
2019-07-30 17:48:41 +0800

    accomplish question_1

--- Question1/GoldenCoins.cpp
+++ Question1/GoldenCoins.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
+int getGoldenCount(int N) {
+	int count = 0;
+	int k = 1, days = 0;
+
+	while ((days + k) <= N) {
+		count += k * k;
+		days += k;
+		k++;
+	}
+	int j = N - days;
+    count += k * j;
+
+	return count;
+}
+
 int main(){
-    ifstream inputdata("Get the Inversion/inputdata.txt"); //输入文件流对象
-    ofstream outputdata("Get the Inversion/outputdata.txt") //输出文件流对象
-    int N = 0;
+    ifstream inputdata("inputdata.txt", ios::in); //输入文件流对象
+    ofstream outputdata("outputdata.txt", ios::out); //输出文件流对象
+    int N = 0, result = 0;
     inputdata>>N;
     while(N>0){
-        vector<int> A(N);
-        for(int i = 0; i < N; i++){
-            inputdata>>A[i];
-        }
+        result = getGoldenCount(N);
+		cout << result << endl;
+		outputdata << N << " " << result << endl;
+		inputdata >> N;
     }
+    inputdata.close();
+    outputdata.close();
 
+    return 0;
 }
