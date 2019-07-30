54ee5cab6cb91b716cb99ddfe9fe615d05b4e28d
Mr yang <18728163854@163.com>
2019-07-30 18:14:45 +0800

    add question2

---
+++ Question2/Hangover.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+int getHangOver(float N) {
+	int count = 0;
+	
+
+	return count;
+}
+
+int main(){
+    ifstream inputdata("inputdata.txt", ios::in); //输入文件流对象
+    ofstream outputdata("outputdata.txt", ios::out); //输出文件流对象
+    int result = 0;
+	float N;
+    inputdata>>N;
+    while(N > 0.00){
+       
+    }
+    inputdata.close();
+    outputdata.close();
+
+    return 0;
+}
